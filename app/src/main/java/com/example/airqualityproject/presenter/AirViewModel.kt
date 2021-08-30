d98fe37a3e2da78e400cf4c030ab0b7b0ffa9569
Henrique <henrique.tavolaro@gmail.com>
2021-08-30 18:40:39 -0300

    charts working

--- app/src/main/java/com/example/airqualityproject/presenter/AirViewModel.kt
+++ app/src/main/java/com/example/airqualityproject/presenter/AirViewModel.kt
@@ -1,10 +1,19 @@
 package com.example.airqualityproject.presenter
 
-import android.util.Log
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.airqualityproject.domain.model.details.ResponseDetails
 import com.example.airqualityproject.domain.model.search.Data
 import com.example.airqualityproject.domain.model.search.Response
 import com.example.airqualityproject.domain.repositories.AirRepository
+import com.example.airqualityproject.presenter.detail_fragment.Pollutants
+import com.github.aachartmodel.aainfographics.aachartcreator.AAOptions
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
+import com.github.aachartmodel.aainfographics.aaoptionsmodel.AAChart
+import com.github.aachartmodel.aainfographics.aaoptionsmodel.AATitle
+import com.github.aachartmodel.aainfographics.aaoptionsmodel.AAXAxis
 import kotlinx.coroutines.launch
 
 class AirViewModel(
@@ -12,8 +21,10 @@ class AirViewModel(
     private val token: String
 ) : ViewModel() {
 
+    var selected = ""
+
     private val _result: MutableLiveData<Response> = MutableLiveData()
-    val result: LiveData<Response> get() = _result
+    val result: LiveData<Response> = _result
 
     fun search(city: String) {
         viewModelScope.launch {
@@ -31,17 +42,82 @@ class AirViewModel(
         _navigateToAirQualityDetails.value = data
     }
 
-    fun onCityAirQualityDetailsNavigated(){
-        _navigateToAirQualityDetails.value = null
-    }
+    private val _detailsResult: MutableLiveData<ResponseDetails> = MutableLiveData()
+    val detailsResult: LiveData<ResponseDetails> = _detailsResult
 
-
-
-    fun getDetails(city: String){
+    fun getDetails(city: String) {
         viewModelScope.launch {
-            val result = repository.getDetails(city)
-            Log.d("TAG111", result.toString())
+            _detailsResult.value = repository.getDetails(city)
         }
     }
+
+    fun createChart(result: ResponseDetails): AAOptions {
+        val maxArray = mutableListOf<Int>()
+        val minArray = mutableListOf<Int>()
+        val daysArray = mutableListOf<String>()
+        when (selected) {
+            Pollutants.O3.name -> {
+                for (i in result.data.forecast.daily.o3) {
+                    maxArray.add(i.max)
+                    daysArray.add(i.day)
+                }
+                for (i in result.data.forecast.daily.o3) {
+                    minArray.add(i.min)
+                }
+            }
+            Pollutants.PM10.name -> {
+                for (i in result.data.forecast.daily.pm10) {
+                    maxArray.add(i.max)
+                    daysArray.add(i.day)
+                }
+                for (i in result.data.forecast.daily.pm10) {
+                    minArray.add(i.min)
+                }
+            }
+            Pollutants.PM25.name -> {
+                for (i in result.data.forecast.daily.pm25) {
+                    maxArray.add(i.max)
+                    daysArray.add(i.day)
+                }
+                for (i in result.data.forecast.daily.pm25) {
+                    minArray.add(i.min)
+                }
+            }
+            Pollutants.UVI.name -> {
+                for (i in result!!.data.forecast.daily.uvi) {
+                    maxArray.add(i.max)
+                    daysArray.add(i.day)
+                }
+                for (i in result!!.data.forecast.daily.uvi) {
+                    minArray.add(i.min)
+                }
+            }
+        }
+
+        val aaXAxis = AAXAxis()
+            .visible(true)
+            .min(0f)
+            .categories(daysArray.toTypedArray())
+
+        return AAOptions()
+            .chart(AAChart().backgroundColor("#ffffff"))
+            .title(AATitle().text("$selected Forecast"))
+            .xAxis(aaXAxis)
+            .series(
+                arrayOf(
+                    AASeriesElement()
+                        .name("Max")
+                        .data(
+                            maxArray.toTypedArray()
+                        ),
+                    AASeriesElement()
+                        .name("Min")
+                        .data(
+                            minArray.toTypedArray()
+                        ),
+                )
+            )
+    }
+
 
 }
--- app/src/main/java/com/example/airqualityproject/presenter/detail_fragment/DetailFragment.kt
+++ app/src/main/java/com/example/airqualityproject/presenter/detail_fragment/DetailFragment.kt
@@ -6,22 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import com.example.airqualityproject.R
 import com.example.airqualityproject.databinding.FragmentDetailBinding
+import com.example.airqualityproject.domain.model.details.ResponseDetails
 import com.example.airqualityproject.presenter.AirViewModel
+import com.github.aachartmodel.aainfographics.aachartcreator.*
+import com.github.aachartmodel.aainfographics.aaoptionsmodel.AAChart
+import com.github.aachartmodel.aainfographics.aaoptionsmodel.AATitle
+import com.github.aachartmodel.aainfographics.aaoptionsmodel.AAXAxis
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class DetailFragment : Fragment()  {
+class DetailFragment : Fragment() {
     val args: DetailFragmentArgs by navArgs()
     private lateinit var binding: FragmentDetailBinding
-    private lateinit var mMap: GoogleMap
     private val viewModel: AirViewModel by viewModel()
 
     override fun onCreateView(
@@ -34,22 +38,56 @@ class DetailFragment : Fragment()  {
             container,
             false
         )
-
-        viewModel.getDetails("@${args.data.uid}")
+        var selected = ""
 
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
 
+        val aaChartView = binding.aaChartView
 
+
+        binding.o3.setOnClickListener {
+            viewModel.selected = Pollutants.O3.name
+            viewModel.getDetails("@${args.data.uid}")
+        }
+
+        binding.pm10.setOnClickListener {
+            viewModel.selected = Pollutants.PM10.name
+            viewModel.getDetails("@${args.data.uid}")
+        }
+
+        binding.pm25.setOnClickListener {
+            viewModel.selected = Pollutants.PM25.name
+            viewModel.getDetails("@${args.data.uid}")
+        }
+
+        binding.uvi.setOnClickListener {
+            viewModel.selected = Pollutants.UVI.name
+            viewModel.getDetails("@${args.data.uid}")
+        }
+
+        viewModel.detailsResult.observe(viewLifecycleOwner, Observer {
+            val createChart = viewModel.createChart(it!!)
+            aaChartView.aa_drawChartWithChartOptions(createChart)
+        })
         return binding.root
     }
+
     private val callback = OnMapReadyCallback { googleMap ->
         val lat = args.data.station.geo[0]
         val lon = args.data.station.geo[1]
-        val location = LatLng(lat,lon)
+        val location = LatLng(lat, lon)
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 14f))
         googleMap.addMarker(MarkerOptions().position(location).title(args.data.station.name))
     }
-
-
 }
+
+
+enum class Pollutants {
+    O3,
+    PM10,
+    PM25,
+    UVI
+}
+
+
